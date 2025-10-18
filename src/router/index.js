2e844edbb83ebd5d0b551baf945d1c220dbdb09b
Muhammed Thanni Akanbi <thann2019@gmail.com>
2025-10-18 20:05:40 +0100

    Fix routing for GitHub Pages by switching to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Courses from '../views/Courses.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
